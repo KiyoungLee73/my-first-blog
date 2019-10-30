015a928bbcfcb090bacb942e22e04dbba84b6e9f
KiyoungLee73 <viracoch@naver.com>
2019-10-30 11:45:50 +0900

    Bookmark App Add

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import *
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('bookmark/', include('bookmark.urls'))
+    path('', BookmarkListView.as_view(), name='list'),
+    path('add/', BookmarkCreateView.as_view(), name='add'),
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', BookmarkUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),
 ]
